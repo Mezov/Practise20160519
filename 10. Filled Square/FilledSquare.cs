9fc26e6b24f2c75d04ab3529b4850f2142eeb136
Mezov <andrey.mezov@gmail.com>
2016-05-21 14:02:02 +0300

    Add projects

---
+++ 10. Filled Square/FilledSquare.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class FilledSquare
+    {    
+    static void Main(string[] args)
+        {
+        
+        int n = int.Parse(Console.ReadLine());
+
+        printHeader(n);
+        printMidle(n);
+        printHeader(n);
+
+        }
+    static void printHeader(int n)
+        {
+        Console.WriteLine("{0}", new string('-', 2 * n));
+        }
+    static void printMidle(int n)
+        {
+        for (int a = 0; a < n - 2; a++)
+            {
+            Console.Write("-");
+            for (int i = 0; i < n - 1; i++)
+                {
+                Console.Write(@"\/");
+                }
+            Console.Write("-");
+            Console.WriteLine();
+            }
+        }
+    }
+
---
+++ SomePractise/SomePractise.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class SomePractise
+    {
+    private static object printHeader;
+
+    static void Main(string[] args)
+        {//https://judge.softuni.bg/Contests/Practice/Index/171#0
+        // Test na Git Hub 
+
+        //01. Centuries to Minutes
+        //Console.Write("Centuries = ");
+        //int centuries = int.Parse(Console.ReadLine());
+        //int years = centuries * 100;
+        //int days = (int)(years * 365.2422);
+        //int hours = 24 * days;
+        //int minutes = 60 * hours;
+        //Console.WriteLine("{0} centuries = {1} years = {2} days = {3} hours = {4} minutes", centuries, years, days, hours, minutes);
+
+        ////02. Circle Area Roundet 12 
+        //double r = double.Parse(Console.ReadLine());
+        //Console.WriteLine("{0:f12}", Math.PI*r*r);
+
+        ////3.	Exact Sum of Real Numbers
+        //int n = int.Parse(Console.ReadLine());
+        //decimal result = 0;
+        //for (int i = 0; i < n; i++)
+        //    {
+        //    result += decimal.Parse(Console.ReadLine());
+        //    }
+        //Console.WriteLine(result);
+
+        // 04.Elevator
+        //int p = int.Parse(Console.ReadLine());
+        //int c = int.Parse(Console.ReadLine());
+        //int result = (int)Math.Ceiling((double)p / c);
+        //Console.WriteLine(result);
+
+        //5.	Special Numbers
+
+        //int n = int.Parse(Console.ReadLine());
+
+        //for (int i = 1; i <= n; i++)
+        //    {
+        //    int sumOfDigits = 0;
+        //    int number = i;
+        //    while (number>0)
+        //        {
+        //        sumOfDigits += number % 10;
+        //        number = number / 10;
+        //        }
+        //    bool isSpecial = (sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11);           
+
+        //        Console.WriteLine("{0} -> {1}",i, isSpecial);               
+
+
+        //    }
+
+        //6.	Triples of Latin Letters
+
+        //int n = int.Parse(Console.ReadLine());
+        //for (int i1 = 0; i1 < n; i1++)
+        //    for (int i2 = 0; i2 < n; i2++)
+        //        for (int i3 = 0; i3 < n; i3++)
+        //            {
+        //            char letter1 = (char)('a' + i1);
+        //            char letter2 = (char)('a' + i2);
+        //            char letter3 = (char)('a' + i3);
+        //            Console.WriteLine("{0}{1}{2}",
+        //              letter1, letter2, letter3);
+        //            }
+
+
+        //7.Greeting
+        //string firstName = Console.ReadLine();
+        //string lastName  = Console.ReadLine();
+        //int age          = int.Parse(Console.ReadLine());
+
+        //Console.WriteLine($"Hello, {firstName} {lastName}.You are {age} years old.");
+
+        //08. Day of Week 
+        //int day = int.Parse(Console.ReadLine());
+
+        //switch (day)
+        //    {
+        //    case 1: Console.WriteLine("Monday");break;
+        //    case 2: Console.WriteLine("Tuesday");break;
+        //    case 3: Console.WriteLine("Wednesday"); break;
+        //    case 4: Console.WriteLine("Thursday"); break;
+        //    case 5: Console.WriteLine("Friday"); break;
+        //    case 6: Console.WriteLine("Saturday"); break;
+        //    case 7: Console.WriteLine("Sunday"); break;
+        //    default: Console.WriteLine("Error"); break;               
+        //    }
+
+        //09. Animal Type 
+        //string input = Console.ReadLine();
+        //if (input=="dog")
+        //    {
+        //    Console.WriteLine("mammal");
+        //    }
+        //else if (input=="crocodile" || input=="tortoise" || input =="snake")
+        //    {
+        //    Console.WriteLine("reptile");
+        //    }
+        //else
+        //    {
+        //    Console.WriteLine("unknown");
+        //    }
+
+        //10. Filled Square ===> in first project 
+        
+       
+
+
+
+        }
+
+    }
+
